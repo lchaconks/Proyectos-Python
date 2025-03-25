5d5ded867386cf10077f53fff2de98a37d381874
lchaconks <lchacon.ks@gmail.com>
2025-03-25 15:47:16 -0400

    Update

---
+++ Totorial/src/Tutorial_02/tipos/01-variables.py
@@ -0,0 +1,10 @@
+nombre_curso = "Ultimate Python"
+nombre1 = "hola"
+NOMBRE_CURSO = "Mundo"
+NoMbRe_CuRsO = "Chanchito"
+NombrCurso = "Feliz"
+print(nombre_curso, nombre1, NOMBRE_CURSO, NoMbRe_CuRsO, NombrCurso)
+
+alumnos = 5000
+puntaje = 9.9
+Publicado = False
