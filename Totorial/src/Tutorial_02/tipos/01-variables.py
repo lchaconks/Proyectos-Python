957fbc519f74ff73b8afcb19c21b224c2793ef2f
lchaconks <lchacon.ks@gmail.com>
2025-03-25 15:54:28 -0400

    Update

--- Totorial/src/Tutorial_02/tipos/01-variables.py
+++ Totorial/src/Tutorial_02/tipos/01-variables.py
@@ -2,9 +2,9 @@ nombre_curso = "Ultimate Python"
 nombre1 = "hola"
 NOMBRE_CURSO = "Mundo"
 NoMbRe_CuRsO = "Chanchito"
-NombrCurso = "Feliz"
+NombreCurso = "Feliz"
 print(nombre_curso, nombre1, NOMBRE_CURSO, NoMbRe_CuRsO, NombrCurso)
 
 alumnos = 5000
-puntaje = 9.9
+puntos = 9.9
 Publicado = False
